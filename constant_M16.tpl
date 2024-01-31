9f9d652dbdd4cffa6ab86864d3195cbb45a8845f
thuisman21 <49718714+thuisman21@users.noreply.github.com>
2024-01-31 16:15:24 +0100

    .tpl file can now be generated

---
+++ constant_M16.tpl
@@ -0,0 +1,39 @@
+//Number of population samples (demes)
+4 populations to simulate
+//Population effective sizes (number of genes)
+N_POP0
+N_POP1
+N_POP2
+N_POP3
+//Sample sizes
+12
+12
+12
+12
+//Growth rates
+0
+0
+0
+0
+//Number of migration matrices
+4
+//Migration matrix 0
+0.000 0.000 0.000 0.000 
+0.000 0.000 0.000 0.000 
+0.000 0.000 0.000 0.000 
+0.000 0.000 0.000 0.000 
+//Migration matrix 1
+0.000 0.000 0.000 0.000 
+0.000 0.000 0.000 0.000 
+0.000 0.000 0.000 0.000 
+0.000 0.000 0.000 0.000 
+//Migration matrix 2
+0.000 0.000 0.000 0.000 
+0.000 0.000 0.000 0.000 
+0.000 0.000 0.000 0.000 
+0.000 0.000 0.000 0.000 
+//Migration matrix 3
+0.000 0.000 0.000 0.000 
+0.000 0.000 0.000 0.000 
+0.000 0.000 0.000 0.000 
+0.000 0.000 0.000 0.000 
--- inputFileGenerator.py
+++ inputFileGenerator.py
@@ -1,8 +1,11 @@
 import argparse
+from _ast import arguments
 
 
 def argument_list():
     parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", required=True, type=str, help="File prefix")
+    parser.add_argument("-i", "--input", default="", type=str, help="Input file for matrices")
     parser.add_argument("-n", "--n_pops", required=True, type=int, help="Number of populations")
     parser.add_argument("-s", "--sample_size", required=True, type=int, help="Sample size of the populations")
     parser.add_argument("-g", "--growth_rate", default=0, type=int, help="Growth rates of the populations")
@@ -12,8 +15,46 @@ def argument_list():
     return args
 
 
+def create_tpl(arguments):
+    output_file = arguments.file + ".tpl"
+    with open(output_file, "w") as f:
+        f.write(f"//Number of population samples (demes)\n")
+        f.write(f"{arguments.n_pops} populations to simulate\n")
+
+        f.write(f"//Population effective sizes (number of genes)\n")
+        for i in range(arguments.n_pops):
+            f.write(f"N_POP{i}\n")
+
+        f.write(f"//Sample sizes\n")
+        for i in range(arguments.n_pops):
+            f.write(f"{arguments.sample_size}\n")
+
+        f.write(f"//Growth rates\n")
+        for i in range(arguments.n_pops):
+            f.write(f"{arguments.growth_rate}\n")
+
+        f.write(f"//Number of migration matrices\n")
+        f.write(f"{arguments.n_pops}\n")
+
+        if arguments.input == "":
+            for i in range(arguments.n_pops):
+                f.write(f"//Migration matrix {i}\n")
+                for j in range(arguments.n_pops):
+                    for k in range(arguments.n_pops):
+                        f.write(f"0.000 ")
+                    f.write(f"\n")
+
+
+#def create_est(argument):
+
+
+
+
 def main():
     arg_list = argument_list()
+
+    create_tpl(arg_list)
+    #create_est(arg_list)
 
 
 if __name__ == "__main__":
